93da763f8307e2bf3c97cb2cc0ca73bd4224f494
alex <alexandr.zolotov@thomascookonline.com>
2017-01-31 10:22:59 +0200

    initial commit

---
+++ src/main/java/com/thomascook/Runner.java
@@ -0,0 +1,13 @@
+package com.thomascook;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Runner
+{
+    public static void main( String[] args )
+    {
+
+
+    }
+}
